831ec48e8693e36fa4a6e4951a87344a2c276c96
StevenCalu <estevenca8@gmail.com>
2026-05-28 19:56:54 -0500

    crear entidad jpa para cursos

---
+++ src/main/java/com/espe/EXM_1P/persistence/CursoEntity.java
@@ -0,0 +1,57 @@
+package com.espe.EXM_1P.persistence;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tbl_cursos_base")
+public class CursoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_curso")
+    private Long idCurso;
+
+    @Column(
+            name = "nombre_curso", nullable = false, unique = true, length = 120
+    )
+    private String nombreCurso;
+
+    @Column(name = "precio_usd")
+    private BigDecimal precioUsd;
+
+    @Column(name = "horas_duracion")
+    private Integer horasDuracion;
+
+    public Long getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(Long idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+
+
+    public void setNombreCurso(String nombreCurso) {
+        this.nombreCurso = nombreCurso;
+    }
+
+    public BigDecimal getPrecioUsd() {
+        return precioUsd;
+    }
+
+    public void setPrecioUsd(BigDecimal precioUsd) {
+        this.precioUsd = precioUsd;
+    }
+
+    public Integer getHorasDuracion() {
+        return horasDuracion;
+    }
+
+    public void setHorasDuracion(Integer horasDuracion) {
+        this.horasDuracion = horasDuracion;
+    }
+}
