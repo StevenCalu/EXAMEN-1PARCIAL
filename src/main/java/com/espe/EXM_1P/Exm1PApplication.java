cda008132621ebb6b83f6faae7087dd2f308fcd6
StevenCalu <estevenca8@gmail.com>
2026-05-28 19:49:24 -0500

    configurar perfil prod y db edusmart

---
+++ src/main/java/com/espe/EXM_1P/Exm1PApplication.java
@@ -0,0 +1,13 @@
+package com.espe.EXM_1P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exm1PApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exm1PApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espe/EXM_1P/Exm1PApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espe.EXM_1P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exm1PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
